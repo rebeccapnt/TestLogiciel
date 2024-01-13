cbde905888749e4b93e3f218c1006549488568d2
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2024-01-13 10:48:17 +0100

    [feat] Add tests

--- i3gl-project/src/test/java/tempguardiantests/AlertManagerTest.java
+++ i3gl-project/src/test/java/tempguardiantests/AlertManagerTest.java
@@ -1,10 +1,8 @@
 package tempguardiantests;
 
+import exceptions.WeatherException;
 import managers.AlertManager;
-import models.Address;
-import models.AlertData;
-import models.Threshold;
-import models.User;
+import models.*;
 import models.enums.ThresholdEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +13,8 @@ import services.IAlertWriter;
 import services.IWeatherAgent;
 import services.WeatherAgent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,9 +38,12 @@ public class AlertManagerTest {
 
     private Threshold thresholdWind;
     private Threshold thresholdWindMinMaxReached;
-    private final Double VALUE_MIN = 0.0;
-    private final Double VALUE_MAX = 20.0;
-    private final String firstAddress = "2 Rue du Pré Nian, 44000 Nantes";
+    private final double VALUE_MIN = 0.0;
+    private final double VALUE_MAX = 20.0;
+    private final double VALUE_1 = 10;
+    private final double VALUE_2 = 15;
+    private final String firstAddress = "15 Rue de la Paix, Paris";
+    private final Location firstLocation = new Location(2.3312846, 48.8695088);
     private final String secondAddress = "29 Rue Saint-Antoine, Paris";
     private final String thirdAddress = "Place Bellecour, 69002 Lyon, France";
     private final String username = "Tintin";
@@ -56,7 +59,6 @@ public class AlertManagerTest {
         alertWriterMock = mock(IAlertWriter.class);
         userRepository = new UserRepository();
         alertManager = new AlertManager(userRepository, weatherAgentMock, alertWriterMock);
-
         //Threshold
         thresholdTemperatureMaxReached = new Threshold(ThresholdEnum.TEMPERATURE, VALUE_MIN, Double.MIN_VALUE);
         thresholdTemperatureMinReached = new Threshold(ThresholdEnum.TEMPERATURE, Double.MAX_VALUE, VALUE_MAX);
@@ -72,7 +74,7 @@ public class AlertManagerTest {
 
     @DisplayName("Not alert sent when test alert disabled for the user and addresses enabled")
     @Test
-    void should_not_alert_sent_when_test_alert_disabled_for_user_adresses_enabled() {
+    void should_not_alert_sent_when_test_alert_disabled_for_user_adresses_enabled() throws WeatherException, InterruptedException {
         //Arrange
         Address address = new Address(firstAddress, true, List.of(thresholdWindMinMaxReached));
         User user = new User(username);
@@ -82,6 +84,54 @@ public class AlertManagerTest {
         alertManager.checkAlert();
         //Assert
         verify(alertWriterMock, never()).writeAlert(any(AlertData.class));
+    }
+
+    @DisplayName("Not alert sent when test alert disabled for the user and addresses disabled")
+    @Test
+    void should_not_alert_sent_when_test_alert_disabled_for_user_adresses_disabled() throws WeatherException, InterruptedException {
+        //Arrange
+        Address address = new Address(firstAddress, false, List.of(thresholdWindMinMaxReached));
+        User user = new User(username);
+        user.addAddress(address);
+        user.setDisableAllAlerts(true);
+        //Act
+        alertManager.checkAlert();
+        //Assert
+        verify(alertWriterMock, never()).writeAlert(any(AlertData.class));
+    }
+
+    @DisplayName("Not alert sent when test alert enabled for the user and addresses disabled")
+    @Test
+    void should_alert_sent_when_test_alert_enabled_for_user_adresses_disabled() throws WeatherException, InterruptedException {
+        //Arrange
+        Address address = new Address(firstAddress, false, List.of(thresholdWindMinMaxReached));
+        User user = new User(username);
+        user.addAddress(address);
+        user.setDisableAllAlerts(false);
+        //Act
+        alertManager.checkAlert();
+        //Assert
+        verify(alertWriterMock, never()).writeAlert(any(AlertData.class));
+    }
+
+    @DisplayName("Not alert sent when test alert enabled for the user and addresses disabled")
+    @Test
+    void should_alert_sent_when_test_alert_enabled_for_user_adresses_enabled() throws WeatherException, InterruptedException {
+        //Arrange
+        ArrayList<Threshold>  thresholdArrayList = new ArrayList<>(List.of(thresholdWindMinMaxReached));
+        ArrayList<ThresholdEnum> thresholdsEnumList =  new ArrayList<>(List.of(ThresholdEnum.WIND));
+        HashMap<ThresholdEnum, Double> resultWeatherAPI = new HashMap<>();
+        resultWeatherAPI.put(ThresholdEnum.WIND, VALUE_1);
+        Address address = new Address(firstAddress, false, thresholdArrayList);
+        User user = new User(username);
+        user.addAddress(address);
+        user.setDisableAllAlerts(false);
+        when(.getValuesFromData(firstLocation, thresholdsEnumList)).thenReturn(resultWeatherAPI);
+        when(weatherAgentMock.getValuesFromData(firstLocation, thresholdsEnumList)).thenReturn(resultWeatherAPI);
+        //Act
+        alertManager.checkAlert();
+        //Assert
+        verify(alertWriterMock, times(1)).writeAlert(any(AlertData.class));
     }
 
 
